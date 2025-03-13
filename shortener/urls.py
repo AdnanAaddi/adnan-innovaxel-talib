b27ec8f4af295271dcecd751e095b535d3ead304
Adnan1 <m.adnan.sc@gmail.com>
2025-03-13 16:44:12 +0500

    implement RetrieveOriginalURL

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from shortener.views import CreateShortUrl
+from shortener.views import CreateShortUrl, RetrieveOriginalURL
 urlpatterns = [
     path('shorten/', CreateShortUrl.as_view()),
+    path('shorten/<str:short_code>/', RetrieveOriginalURL.as_view()),
 
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -4,6 +4,7 @@ from .models import ShortenedURL
 from .serializers import URLShortenerSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class CreateShortUrl(APIView):
@@ -14,4 +15,15 @@ class CreateShortUrl(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class RetrieveOriginalURL(APIView):
+    """API to retrieve the original URL from a short code."""
+    def get(self, request, short_code):
+        url_entry = get_object_or_404(ShortenedURL, short_code=short_code)
+        url_entry.access_count += 1
+        url_entry.save()
+        data = URLShortenerSerializer(url_entry).data
+        data.pop("access_count",None)
+        return Response(data)
+
         
