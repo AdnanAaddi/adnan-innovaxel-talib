3c0fa09196e7daf3dfd98a211f08409e9130b9f3
Adnan1 <m.adnan.sc@gmail.com>
2025-03-13 16:04:41 +0500

    Implement msql configuration

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shortener',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'url_shortener',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
