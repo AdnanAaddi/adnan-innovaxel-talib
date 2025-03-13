a20c4b7c8ad6de4b6694b5f68b7987be39aeaefd
Adnan1 <m.adnan.sc@gmail.com>
2025-03-13 16:28:44 +0500

    implement createshorturl

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import ShortenedURL
+from .serializers import URLShortenerSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class CreateShortUrl(APIView):
+    """" Api to create a Short Url"""
+    def post(self , request):
+        serializer = URLShortenerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
